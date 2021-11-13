6e69374d9bde88ac77a1df7056ae1a7620de76a8
Jason Fleischer <jason_fleischer@hotmail.ca>
2021-11-13 10:09:25 -0500

    update

--- js/main.js
+++ js/main.js
@@ -79,14 +79,14 @@ const callbackExample = data => {
 	function getCentsColor(cents) {
 
 		var c = Math.abs(parseInt(cents));
-		if (c <= 10) { // green to yellow
+		if (c <= 9) { // green to yellow
 			let number = parseInt(255 * ((0-c)/10));
 			var redValueHexStr = number.toString(16);
 			if(redValueHexStr < 10) {
 				redValueHexStr = "0" + redValueHexStr;
 			}
 			return "#" + redValueHexStr+ "ff00";
-		} else if (c > 10 && c <= 25) { // yellow to red (10 to 25)
+		} else if (c > 9 && c <= 25) { // yellow to red (10 to 25)
 			let number = parseInt(255 * ((15-(c-10))/15));
 			var greenValueHexStr = number.toString(16);
 			if(greenValueHexStr < 10) {
