e2099eccb0fbbc97ec57af7946b337f0681cdf35
Jason Fleischer <jason_fleischer@hotmail.ca>
2021-11-16 20:35:47 -0500

    update

--- js/tuner_view.js
+++ js/tuner_view.js
@@ -80,7 +80,7 @@ class TunerView {
 			var note = musicKit.all_notes[i];
 		
 			ctx.fillStyle = '#050505';
-	    	ctx.font = (this.HEIGHT * 0.1) + 'px san-serif';
+	    	ctx.font = (this.HEIGHT * 0.5) + 'px san-serif';
 	    	ctx.textAlign = 'center';
 	    	ctx.fillText(note.note_name.type, xPosition, 10, this.HEIGHT*0.2);
 	    	xPosition = xPosition + spacing
