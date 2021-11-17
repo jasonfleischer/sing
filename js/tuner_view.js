728dc586a2f6fc615494873e440b92912e213bdd
Jason Fleischer <jason_fleischer@hotmail.ca>
2021-11-16 20:37:19 -0500

    update

--- js/tuner_view.js
+++ js/tuner_view.js
@@ -82,7 +82,7 @@ class TunerView {
 			ctx.fillStyle = '#00f';
 	    	ctx.font = (this.HEIGHT * 0.5) + 'px san-serif';
 	    	ctx.textAlign = 'center';
-	    	ctx.fillText(note.note_name.type, xPosition, 10, this.HEIGHT*0.4);
+	    	ctx.fillText(note.note_name.type, xPosition, 10, this.HEIGHT*0.5);
 	    	xPosition = xPosition + spacing
 		}
 
