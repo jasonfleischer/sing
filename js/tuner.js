093fcc05835bb90460aae8e2960f47d00ff29c89
Jason Fleischer <jason_fleischer@hotmail.ca>
2021-11-18 20:45:35 -0500

    update

--- js/tuner.js
+++ js/tuner.js
@@ -21,7 +21,7 @@ const tuner = {
 
 /*'use strict'
 
-*/
+
 const USER_MEDIA_CONSTRAINTS = {
   audio: {
     mandatory: {
@@ -41,7 +41,7 @@ const CONCERT_PITCH = 440 //frequency of a fixed note, which is used as a standa
 const MIDI = 69 // the MIDI note number of A4
 const A = 2 ** (1 / 12) // the twelth root of 2 = the number which when multiplied by itself 12 times equals 2 = 1.059463094359...
 const C0_PITCH = 16.35 // frequency of lowest note: C0
-
+*/
 //import { USER_MEDIA_CONSTRAINTS, FFT_SIZE } from './constants.js'
 
 
@@ -101,14 +101,14 @@ tuner.autoCorrelate = (buf, sampleRate) => {
 
 
 tuner.getDataFromFrequency = (frequency) => {
-  const N = Math.round(12 * Math.log2(frequency / CONCERT_PITCH)) // the number of half steps away from the fixed note you are. If you are at a higher note, n is positive. If you are on a lower note, n is negative.
-  const Fn = CONCERT_PITCH * A ** N // the frequency of the note n half steps away of concert pitch
-  const noteIndex = (N + MIDI) % 12 // index of note letter from NOTES array
-  const octave = Math.floor(Math.log2(Fn / C0_PITCH))
+  const N = Math.round(12 * Math.log2(frequency / tuner.CONCERT_PITCH)) // the number of half steps away from the fixed note you are. If you are at a higher note, n is positive. If you are on a lower note, n is negative.
+  const Fn = tuner.CONCERT_PITCH * tuner.A ** N // the frequency of the note n half steps away of concert pitch
+  const noteIndex = (N + tuner.MIDI) % 12 // index of note letter from NOTES array
+  const octave = Math.floor(Math.log2(Fn / tuner.C0_PITCH))
 
   return {
     frequency,
-    note: NOTES[noteIndex],
+    note: tuner.NOTES[noteIndex],
     noteFrequency: Fn,
     deviation: frequency - Fn,
     octave,
@@ -125,16 +125,16 @@ tuner.freelizer = async () => {
 
   const init = async () => {
     const stream = await navigator.mediaDevices.getUserMedia(
-      USER_MEDIA_CONSTRAINTS
+      tuner.USER_MEDIA_CONSTRAINTS
     )
     audioContext = new AudioContext()
     analyser = audioContext.createAnalyser()
-    analyser.fftSize = FFT_SIZE
+    analyser.fftSize = tuner.FFT_SIZE
     audioContext.createMediaStreamSource(stream).connect(analyser)
   }
 
   const update = () => {
-    const buffer = new Float32Array(FFT_SIZE)
+    const buffer = new Float32Array(tuner.FFT_SIZE)
     analyser.getFloatTimeDomainData(buffer)
     const frequency = tuner.autoCorrelate(buffer, audioContext.sampleRate)
     callbacks.forEach((fn) =>
