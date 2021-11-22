82ffe6680f1b2fe8edd4dd709115694017ada7bf
Jason Fleischer <jason_fleischer@hotmail.ca>
2021-11-21 20:12:50 -0500

    update

--- js/cents_view.js
+++ js/cents_view.js
@@ -63,7 +63,7 @@ class CentsView {
 			ctx.strokeStyle = this.getLineColor(i-50);
 			ctx.lineWidth = 1;
 
-			let dividerHeight = (i % 10 == 0) ? this.HEIGHT*0.9 : this.HEIGHT*0.5;
+			let dividerHeight = (i % 10 == 0) ? 0 : this.HEIGHT*0.5;
 
 			ctx.moveTo(xPosition, dividerHeight);
 			ctx.lineTo(xPosition, this.HEIGHT);
