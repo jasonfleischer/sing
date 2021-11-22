7f307a2dc06d67a513961fe95b5b2c1b42da9b1e
Jason Fleischer <jason_fleischer@hotmail.ca>
2021-11-21 19:57:49 -0500

    update

--- js/cents_view.js
+++ js/cents_view.js
@@ -90,7 +90,7 @@ class CentsView {
 
 	getLineColor(cents) {
 
-		let number = parseInt(255 * ((50-Math.abs(cents))/50));
+		let number = Math.max(0, parseInt(255 * ((50-Math.abs(cents))/50)));
 		var valueHexStr = number.toString(16);
 		if(valueHexStr < 10) {
 			valueHexStr = "0" + valueHexStr;
