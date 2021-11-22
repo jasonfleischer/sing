06493d66470dd05bc2b33a54e607b1d90f92340a
Jason Fleischer <jason_fleischer@hotmail.ca>
2021-11-21 20:17:52 -0500

    update

--- js/tuner_view.js
+++ js/tuner_view.js
@@ -58,7 +58,7 @@ class TunerView {
 		ctx.beginPath();
 		ctx.lineWidth = 2;
 		ctx.fillStyle = "#000";
-		ctx.strokeStyle = "#aaa";
+		ctx.strokeStyle = "#333";
 		ctx.rect(0, 0, this.WIDTH, this.HEIGHT);
 		ctx.fill();
 		ctx.stroke();
