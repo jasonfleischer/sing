a66f35887b3600f9fc57dbea47b37f1d782e3226
Jason Fleischer <jason_fleischer@hotmail.ca>
2022-01-08 20:13:24 -0500

    update

---
+++ js/import.js
@@ -0,0 +1,6 @@
+const log = require("@jasonfleischer/log");
+const pianoKit = require("@jasonfleischer/piano");
+const fretboardKit = require("@jasonfleischer/fretboard");
+const musicKit = require("@jasonfleischer/music-model-kit");
+
+musicKit.init();
--- js/main.js
+++ js/main.js
@@ -1,9 +1,4 @@
-const log = require("@jasonfleischer/log");
-const pianoKit = require("@jasonfleischer/piano");
-const fretboardKit = require("@jasonfleischer/fretboard");
-const musicKit = require("@jasonfleischer/music-model-kit");
 
-musicKit.init();
 
 const tunerView = new TunerView("tuner");
 const tunerView2 = new TunerView("tuner2");
