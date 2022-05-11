e9ac5c21e42b6737707a936810f257115c2659b2
Jason Fleischer <jason_fleischer@hotmail.ca>
2022-05-11 08:11:35 -0400

    up

--- node_modules/@jasonfleischer/music-model-kit/lib/prototypes.js
+++
@@ -1,9 +0,0 @@
-
-
-function randomInteger(min, max) { // min and max included 
-	return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-function replaceAll(str, find, replace) {
-	return str.replace(new RegExp(find, 'g'), replace);
-}
--- scripts/build.sh
+++ scripts/build.sh
@@ -9,6 +9,7 @@
 
 CWD=`pwd`
 
+npm install;
 
 uglifycss css/alert.css css/button.css css/header.css css/main.css css/root.css css/select.css css/slider.css > css/bundle.css
 
